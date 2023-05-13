046e8f5ecf056a5e35a64c02cca630c0348ea481
stefhol <hoef.stef@protonmail.ch>
2023-05-13 08:29:44 +0200

    Improve Code

--- src/main/kotlin/model/Person.kt
+++ src/main/kotlin/model/Person.kt
@@ -4,53 +4,80 @@ import jakarta.persistence.Entity
 import jakarta.persistence.*
 
 @Entity
-class Person(@Id val person_id: Long) {
-    lateinit var username: String
-    lateinit var email: String
-    lateinit var password: String
-    lateinit var profile_picture: String
+@Table(name = "persons")
+data class Person(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    val person_id: Long,
+    val username: String,
+    val email: String,
+    val password: String,
+    val profile_picture: String
+)
 
+@Entity
+@Table(name = "persons_skills")
+data class PersonSkill(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "person_skill_id")
+    val person_skill_id,
     @OneToMany()
-    lateinit var languages: MutableList<Language>
-
-    @OneToMany()
-    lateinit var skills: MutableList<Skill>
-
-    @ManyToMany(cascade = [(CascadeType.ALL)], fetch = FetchType.EAGER)
-    var teams: MutableList<Team> = mutableListOf()
-}
+    val languages: MutableList<Language>,
+    @ManyToOne()
+    val skills: Skill,
+    @ManyToOne(cascade = [(CascadeType.ALL)], fetch = FetchType.EAGER)
+    val teams: Teams
+)
 
 @Entity
-class Language(@Id val language_id: Long) {
-    lateinit var name: String
-}
+@Table(name = "languages")
+data class Language(
+    @Id val language_id: Long,
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    val name: String,
+)
 
 @Entity
-class Skill(@Id val skill_id: Long ) {
-    lateinit var name: String
-}
+@Table(name = "skills")
+data class Skill(
+    @Id val skill_id: Long,
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    val name: String
+)
 
 @Entity
-class Room(@Id val room_id:Long) {
+@Table(name = "rooms")
+data class Rooms(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    val room_id: Long,
     @OneToMany
-    lateinit var persons: MutableList<Person>
+    val persons: MutableList<Person>,
     @OneToMany
-    lateinit var messages: MutableList<Message>
-}
+    val messages: MutableList<Messages>,
+)
 
 @Entity
-class Message(@Id val message_id:Long)  {
+@Table(name = "messages")
+data class Messages(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    val message_id: Long,
     @ManyToOne
-    lateinit var person: Person
-    lateinit var date: java.time.LocalDateTime
-    lateinit var message: String
-}
+    val person: Person,
+    val date: java.time.LocalDateTime,
+    val message: String,
+)
 
 @Entity
-class Team(@Id val team_id:Long)   {
-    lateinit var name: String
-    var description: String = ""
-    var picture = ""
-    @ManyToMany(mappedBy = "teams")
-    var persons: MutableList<Person> = mutableListOf()
-}
+@Table(name = "teams")
+data class Teams(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    val team_id: Long,
+    val name: String,
+    val description: String,
+    val picture: String,
+    val persons: MutableList<Person>,
+)
--- src/main/resources/db/migration/create_tables.sql
+++ src/main/resources/db/migration/create_tables.sql
@@ -1,52 +1,61 @@
-CREATE TABLE persons (
-  person_id SERIAL PRIMARY KEY,
-  username VARCHAR(100),
-  first_name VARCHAR(100),
-  last_name VARCHAR(100),
-  email VARCHAR(100),
-  password VARCHAR(255),
-  profile_picture VARCHAR(255)
+CREATE TABLE persons
+(
+    person_id       SERIAL PRIMARY KEY,
+    username        VARCHAR(100),
+    first_name      VARCHAR(100),
+    last_name       VARCHAR(100),
+    email           VARCHAR(100),
+    password        VARCHAR(255),
+    profile_picture VARCHAR(255)
 );
 
-CREATE TABLE skills (
-  skill_id SERIAL PRIMARY KEY,
-  name VARCHAR(100)
+CREATE TABLE skills
+(
+    skill_id SERIAL PRIMARY KEY,
+    name     VARCHAR(100)
 );
 
-CREATE TABLE persons_skills (
-  person_skill_id SERIAL PRIMARY KEY,
-  person_id INT REFERENCES person(person_id),
-  skill_id INT REFERENCES skill(skill_id)
+
+CREATE TABLE persons_skills
+(
+    person_id       INT REFERENCES persons (person_id),
+    skill_id        INT REFERENCES skills (skill_id),
+primary key (person_id, skill_id)
 );
 
-CREATE TABLE languages (
-  language_id SERIAL PRIMARY KEY,
-  name VARCHAR(100)
+CREATE TABLE languages
+(
+    language_id SERIAL PRIMARY KEY,
+    name        VARCHAR(100)
 );
 
-CREATE TABLE persons_languages (
-  person_language_id SERIAL PRIMARY KEY,
-  person_id INT REFERENCES person(person_id),
-  language_id INT REFERENCES language(language_id)
+CREATE TABLE persons_languages
+(
+    person_language_id SERIAL PRIMARY KEY,
+    person_id          INT REFERENCES persons (person_id),
+    language_id        INT REFERENCES languages (language_id)
 );
 
-CREATE TABLE teams (
-  team_id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
-  description TEXT,
-  picture VARCHAR(255)
+CREATE TABLE teams
+(
+    team_id     SERIAL PRIMARY KEY,
+    name        VARCHAR(100),
+    description TEXT,
+    picture     VARCHAR(255)
 );
 
-CREATE TABLE rooms (
-  room_id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
-  team_id INT REFERENCES team(team_id)
+CREATE TABLE rooms
+(
+    room_id SERIAL PRIMARY KEY,
+    name    VARCHAR(100),
+    team_id INT REFERENCES teams (team_id)
 );
 
-CREATE TABLE messages (
-  message_id SERIAL PRIMARY KEY,
-  content VARCHAR(255),
-  sender_id INT REFERENCES person(person_id),
-  room_id INT REFERENCES room(room_id),
-  created_at TIMESTAMP
+CREATE TABLE messages
+(
+    message_id SERIAL PRIMARY KEY,
+    content    VARCHAR(255),
+    sender_id  INT REFERENCES persons (person_id),
+    room_id    INT REFERENCES rooms (room_id),
+    created_at TIMESTAMP
 );
